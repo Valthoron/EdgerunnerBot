0aef548e118c3e912fa43e3bd784cc8470852794
Serdar Üşenmez <serdar.usenmez@gmail.com>
2022-10-26 19:28:15 +0300

    Clean-up of old data variables.

--- game/data.py
+++ game/data.py
@@ -5,32 +5,23 @@ stats = {}
 skills = {}
 attacks = {}
 
-batch_locs = {}
-batch_namelocs = {}
-
 with open("data/attributes.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         key = row["key"].lower()
         attributes[key] = row
-        batch_locs[row["loc"]] = key
 
 with open("data/stats.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         key = row["key"].lower()
         stats[key] = row
-        batch_locs[row["loc"]] = key
 
 with open("data/skills.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         key = row["key"].lower()
         skills[key] = row
-        batch_locs[row["loc"]] = key
-
-        if row["nameloc"]:
-            batch_namelocs[row["nameloc"]] = key
 
 with open("data/attacks.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
